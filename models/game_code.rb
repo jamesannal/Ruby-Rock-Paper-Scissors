f65817258860cd5ddd39befcbbc1005ce27fc35e
James Annal <jamesannal@gmail.com>
2016-12-13 08:51:34 +0000

    added new descriptions to win message

--- models/game_code.rb
+++ models/game_code.rb
@@ -7,17 +7,17 @@ class Game
 
   def winner
     if @hand1 == 'rock' && @hand2 == 'scissors'
-      return "Left hand's rock beats the right hand's scissors!"
+      return "Left hand's rock smashes right hand's scissors!"
     elsif @hand1 == 'rock' && @hand2 == 'paper'
-      return "Right hand's paper beats left hand's rock!"
+      return "Right hand's paper gives a paper cut to left hand's rock!"
     elsif @hand1 == 'scissors' && @hand2 == 'rock'
-      return "Right hand's rock beats left hand's scissors!"
+      return "Right hand's rock blunts left hand's scissors!"
     elsif @hand1 == 'scissors' && @hand2 == 'paper'
-      return "Left hand's scissors beats right hand's paper!"
+      return "Left hand's scissors decapitates right hand's paper!"
     elsif @hand1 == 'paper' && @hand2 == 'rock'
-      return "Left hand's paper beats right hand's rock!"
+      return "Left hand's paper hides right hand's rock!"
     elsif @hand1 == 'paper' && @hand2 == 'scissors'
-      return "Right hand's scissors beats left hand's paper!"
+      return "Right hand's scissors dismembers left hand's paper!"
     elsif @hand1 == @hand2
       return "A battle for the ages! (draw)"
     else
